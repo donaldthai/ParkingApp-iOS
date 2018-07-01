e1c4eaab44a9228a40aebf4d902e46b3e4a9bfaf
user141581 <user141581@74-80-245-25.ic2net.net>
2018-07-01 12:36:30 -0700

    Initial Commit

---
+++ ParkingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkingApp
+//
+//  Created by user141581 on 7/1/18.
+//  Copyright © 2018 dthai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
